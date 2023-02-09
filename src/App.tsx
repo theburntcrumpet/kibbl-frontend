f910ca4ecde77986cf5e6226af7d5de9bf133ebe
theburntcrumpet <msksansom@gmail.com>
2023-02-09 03:09:23 +0000

    Fixed up FileTree, added some layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { Container, Grid } from '@mui/material'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 import FileTree from './components/FileTree'
 import Markdown from './components/Markdown'
 import SearchBox from './components/SearchBox'
@@ -9,36 +9,23 @@ function App() {
   const [count, setCount] = useState(0)
   const [selectedFile, setSelectedFile] = useState("");
   const [filePaths, setFilePaths] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
 
-  console.log(selectedFile);
-  console.log(filePaths);
   return (
-    <div className="App">
-      <SearchBox setFilePaths={setFilePaths}/>
-      <FileTree selectedFile={selectedFile} setSelectedFile={setSelectedFile} files={filePaths} />
-      <Markdown  path={selectedFile} />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    
+    <Container sx={{minHeight:"100vh"}} disableGutters maxWidth={false}>
+      <Grid container spacing={2}>
+        <Grid item xs={4} xl={6}>
+          <SearchBox setFilePaths={setFilePaths} searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          <FileTree selectedFile={selectedFile} setSelectedFile={setSelectedFile} files={filePaths} searchTerm={searchTerm}/>
+        </Grid>
+        <Grid item xs={8} xl={6}>
+          <Markdown  path={selectedFile} />
+        </Grid>
+      </Grid>
+      
+    </Container>
   )
 }
 
